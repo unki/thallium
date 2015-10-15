bd9424edf95cfd8a158b196688f3be20a2177e0e
Andreas Unterkircher <unki@netshadow.at>
2015-10-15 21:00:35 +0200

    add thallium_message.js

---
+++ public/resources/thallium_message.js
@@ -0,0 +1,35 @@
+/**
+ * This file is part of Thallium.
+ *
+ * Thallium, a PHP-based framework for web applications.
+ * Copyright (C) <2015> <Andreas Unterkircher>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
+var ThalliumMessage = function () {
+    return true;
+
+    var command;
+    var message;
+};
+
+ThalliumMessage.prototype.setCommand = function (command) {
+    this.command = command;
+    return true;
+}
+
+ThalliumMessage.prototype.setMessage = function (message) {
+    this.message = message;
+    return true;
+}
+
+// vim: set filetype=javascript expandtab softtabstop=4 tabstop=4 shiftwidth=4:
