e6eb50cd22e42067c957899fa6252098ff30f1d8
Andreas Unterkircher <unki@netshadow.at>
2015-10-16 07:01:01 +0200

    InstallerController, remove non Thallium relevant tables

--- vendor/Thallium/Controllers/InstallerController.php
+++ vendor/Thallium/Controllers/InstallerController.php
@@ -72,31 +72,6 @@ class InstallerController extends DefaultController
     {
         global $db;
 
-        if (!$db->checkTableExists("TABLEPREFIXarchive")) {
-            $table_sql = "CREATE TABLE `TABLEPREFIXarchive` (
-                `document_idx` int(11) NOT NULL AUTO_INCREMENT,
-                `document_guid` varchar(255) DEFAULT NULL,
-                `document_title` varchar(255) DEFAULT NULL,
-                `document_description` TEXT DEFAULT NULL,
-                `document_file_name` varchar(255) DEFAULT NULL,
-                `document_file_hash` varchar(255) DEFAULT NULL,
-                `document_file_size` int(11) DEFAULT NULL,
-                `document_signing_icon_position` int(11) DEFAULT NULL,
-                `document_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
-                `document_version` varchar(255) DEFAULT NULL,
-                `document_derivation` int(11) DEFAULT NULL,
-                `document_derivation_guid` varchar(255) DEFAULT NULL,
-                `document_signed_copy` varchar(1) DEFAULT NULL
-                PRIMARY KEY (`document_idx`)
-                    )
-                    ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-
-            if ($db->query($table_sql) === false) {
-                $this->raiseError("Failed to create 'archive' table");
-                return false;
-            }
-        }
-
         if (!$db->checkTableExists("TABLEPREFIXaudit")) {
             $table_sql = "CREATE TABLE `TABLEPREFIXaudit` (
                 `audit_idx` int(11) NOT NULL AUTO_INCREMENT,
@@ -115,22 +90,39 @@ class InstallerController extends DefaultController
             }
         }
 
-        if (!$db->checkTableExists("TABLEPREFIXqueue")) {
-            $table_sql = "CREATE TABLE `TABLEPREFIXqueue` (
-                `queue_idx` int(11) NOT NULL AUTO_INCREMENT,
-                `queue_guid` varchar(255) DEFAULT NULL,
-                `queue_file_name` varchar(255) DEFAULT NULL,
-                `queue_file_hash` varchar(255) DEFAULT NULL,
-                `queue_file_size` int(11) DEFAULT NULL,
-                `queue_signing_icon_position` int(11) DEFAULT NULL,
-                `queue_state` varchar(255) DEFAULT NULL,
-                `queue_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
-                PRIMARY KEY (`queue_idx`)
-                    )
-                    ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+        if (!$db->checkTableExists("TABLEPREFIXmessage_bus")) {
+            $table_sql = "CREATE TABLE `TABLEPREFIXmessage_bus` (
+                `msg_idx` int(11) NOT NULL AUTO_INCREMENT,
+                `msg_guid` varchar(255) DEFAULT NULL,
+                `msg_session_id` varchar(255) NOT NULL,
+                `msg_submit_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+                `msg_scope` varchar(255) DEFAULT NULL,
+                `msg_command` varchar(255) NOT NULL,
+                `msg_body` varchar(255) NOT NULL,
+                `msg_value` varchar(255) DEFAULT NULL,
+                `msg_in_processing` varchar(1) DEFAULT NULL,
+                PRIMARY KEY (`msg_idx`)
+                ) ENGINE=MyISAM DEFAULT CHARSET=utf8";
 
             if ($db->query($table_sql) === false) {
-                $this->raiseError("Failed to create 'queue' table");
+                $this->raiseError("Failed to create 'message_bus' table");
+                return false;
+            }
+        }
+
+        if (!$db->checkTableExists("TABLEPREFIXjobs")) {
+            $table_sql = "CREATE TABLE `TABLEPREFIXjobs` (
+                `job_idx` int(11) NOT NULL AUTO_INCREMENT,
+                `job_guid` varchar(255) DEFAULT NULL,
+                `job_session_id` varchar(255) NOT NULL,
+                `job_request_guid` varchar(255) DEFAULT NULL,
+                `job_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+                `job_in_processing` varchar(1) DEFAULT NULL,
+                PRIMARY KEY (`job_idx`)
+                ) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+
+            if ($db->query($table_sql) === false) {
+                $this->raiseError("Failed to create 'jobs' table");
                 return false;
             }
         }
@@ -155,114 +147,6 @@ class InstallerController extends DefaultController
                 return false;
             }
         }
-
-        if (!$db->checkTableExists("TABLEPREFIXkeywords")) {
-            $table_sql = "CREATE TABLE `TABLEPREFIXkeywords` (
-                `keyword_idx` int(11) NOT NULL auto_increment,
-                `keyword_name` varchar(255) default NULL,
-                `keyword_guid` varchar(255) default NULL,
-                PRIMARY KEY  (`keyword_idx`)
-                )
-                ENGINE=MyISAM DEFAULT CHARSET=utf8;";
-
-            if ($db->query($table_sql) === false) {
-                $this->raiseError("Failed to create 'keywords' table");
-                return false;
-            }
-        }
-
-        if (!$db->checkTableExists("TABLEPREFIXassign_keywords_to_document")) {
-            $table_sql = "CREATE TABLE `TABLEPREFIXassign_keywords_to_document` (
-                `akd_idx` int(11) NOT NULL auto_increment,
-                `akd_guid` varchar(255) default NULL,
-                `akd_archive_idx` int(11) NOT NULL,
-                `akd_keyword_idx` int(11) NOT NULL,
-                PRIMARY KEY  (`akd_idx`),
-                UNIQUE KEY `document_keywords` (`akd_archive_idx`,`akd_keyword_idx`),
-                KEY `documents` (`akd_archive_idx`)
-                )
-                ENGINE=MyISAM DEFAULT CHARSET=utf8;";
-
-            if ($db->query($table_sql) === false) {
-                $this->raiseError("Failed to create 'assign_keywords_to_document' table");
-                return false;
-            }
-        }
-
-        if (!$db->checkTableExists("TABLEPREFIXdocument_indices")) {
-            $table_sql = "CREATE TABLE `TABLEPREFIXdocument_indices` (
-                `di_idx` int(11) NOT NULL auto_increment,
-                `di_guid` varchar(255) default NULL,
-                `di_document_idx` int(11) NOT NULL,
-                `di_document_guid` varchar(255) default NULL,
-                `di_text` TEXT default NULL,
-                PRIMARY KEY  (`di_idx`),
-                KEY `document_indices` (`di_document_idx`,`di_document_guid`),
-                FULLTEXT KEY `text` (`di_text`)
-                )
-                ENGINE=MyISAM DEFAULT CHARSET=utf8;";
-
-            if ($db->query($table_sql) === false) {
-                $this->raiseError("Failed to create 'document_indices' table");
-                return false;
-            }
-        }
-
-        if (!$db->checkTableExists("TABLEPREFIXdocument_properties")) {
-            $table_sql = "CREATE TABLE `TABLEPREFIXdocument_properties` (
-                `dp_idx` int(11) NOT NULL auto_increment,
-                `dp_guid` varchar(255) default NULL,
-                `dp_document_idx` int(11) NOT NULL,
-                `dp_document_guid` varchar(255) default NULL,
-                `dp_property` varchar(255) default NULL,
-                `dp_value` varchar(255) default NULL,
-                PRIMARY KEY  (`dp_idx`),
-                KEY `document_properties` (`dp_document_idx`,`dp_document_guid`)
-                )
-                ENGINE=MyISAM DEFAULT CHARSET=utf8;";
-
-            if ($db->query($table_sql) === false) {
-                $this->raiseError("Failed to create 'document_properties' table");
-                return false;
-            }
-        }
-        if (!$db->checkTableExists("TABLEPREFIXmessage_bus")) {
-            $table_sql = "CREATE TABLE `TABLEPREFIXmessage_bus` (
-                `msg_idx` int(11) NOT NULL AUTO_INCREMENT,
-                `msg_guid` varchar(255) DEFAULT NULL,
-                `msg_session_id` varchar(255) NOT NULL,
-                `msg_submit_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
-                `msg_scope` varchar(255) DEFAULT NULL,
-                `msg_command` varchar(255) NOT NULL,
-                `msg_body` varchar(255) NOT NULL,
-                `msg_value` varchar(255) DEFAULT NULL,
-                `msg_in_processing` varchar(1) DEFAULT NULL,
-                PRIMARY KEY (`msg_idx`)
-                ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8";
-
-            if ($db->query($table_sql) === false) {
-                $this->raiseError("Failed to create 'message_bus' table");
-                return false;
-            }
-        }
-
-        if (!$db->checkTableExists("TABLEPREFIXjobs")) {
-            $table_sql = "CREATE TABLE `TABLEPREFIXjobs` (
-                `job_idx` int(11) NOT NULL AUTO_INCREMENT,
-                `job_guid` varchar(255) DEFAULT NULL,
-                `job_session_id` varchar(255) NOT NULL,
-                `job_request_guid` varchar(255) DEFAULT NULL,
-                `job_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
-                `job_in_processing` varchar(1) DEFAULT NULL,
-                PRIMARY KEY (`job_idx`)
-                ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8";
-
-            if ($db->query($table_sql) === false) {
-                $this->raiseError("Failed to create 'jobs' table");
-                return false;
-            }
-        }
-
         if (!$db->getDatabaseSchemaVersion()) {
             if (!$db->setDatabaseSchemaVersion()) {
                 $this->raiseError("DatabaseController:setDatabaseSchemaVersion() returned false!");
