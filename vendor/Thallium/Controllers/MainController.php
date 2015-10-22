3b777699d421dd7a9425921e8269549442a72b2d
Andreas Unterkircher <unki@netshadow.net>
2015-10-22 11:21:09 +0200

    MainController, use HttpRouterController::select() instead of getQuery()

--- vendor/Thallium/Controllers/MainController.php
+++ vendor/Thallium/Controllers/MainController.php
@@ -59,7 +59,10 @@ class MainController extends DefaultController
         if (!$this->isCmdline()) {
             $this->loadController("HttpRouter", "router");
             global $router;
-            $GLOBALS['query'] = $router->getQuery();
+            if (($GLOBALS['query'] = $router->select()) === false) {
+                $this->raiseError(__METHOD__ .'(), HttpRouterController::select() returned false!');
+                return false;
+            }
             global $query;
         }
 
