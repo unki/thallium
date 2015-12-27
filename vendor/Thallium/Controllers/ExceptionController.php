e6e7379aadbb723f7319c7565f326a7c4a9c2368
Andreas Unterkircher <unki@netshadow.at>
2015-12-27 08:30:52 +0100

    ExceptionController, add getText() and getJson() methods

--- vendor/Thallium/Controllers/ExceptionController.php
+++ vendor/Thallium/Controllers/ExceptionController.php
@@ -21,10 +21,55 @@ namespace Thallium\Controllers;
 
 class ExceptionController extends \Exception
 {
-    // custom string representation of object
+    protected $previous;
+
+    public function __construct($message, $captured_exception = null)
+    {
+        parent::__construct($message, null, $captured_exception);
+    }
+
+    public function getText()
+    {
+        $text = "";
+        if ($previous = $this->getPrevious()) {
+            $text.= "<br /><br />". str_replace("\n", "<br />\n", $previous->getMessage()) ."<br /><br />\n";
+            $text.= "Backtrace:<br />\n". str_replace("\n", "<br />\n", $previous->getTraceAsString());
+            return $text;
+        }
+
+        $text.= "<br /><br />". str_replace("\n", "<br />\n", $this->getMessage()) ."<br /><br />\n";
+        $text.= "Backtrace:<br />\n". str_replace("\n", "<br />\n", parent::getTraceAsString());
+        return $text;
+    }
+
+    public function getJson()
+    {
+        $text = array();
+
+        if ($previous = $this->getPrevious()) {
+            $text = $previous->getMessage();
+            $trace = $previous->getTraceAsString();
+        } else {
+            $text = $this->getMessage();
+            $trace = parent::getTraceAsString();
+        }
+
+        if (($json = json_encode(array('text' => $text, 'trace' => $trace))) === false) {
+            exit("json_encode() failed!");
+        }
+
+        return $json;
+    }
+
     public function __toString()
     {
-        return "Backtrace:<br />\n". str_replace("\n", "<br />\n", parent::getTraceAsString());
+        global $router;
+
+        if ($router->isRpcCall()) {
+            return $this->getJson();
+        }
+
+        return $this->getText();
     }
 }
 
