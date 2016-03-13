a56e12b7e058cd422a1f386cda0ccb00309aa938
Andreas Unterkircher <unki@netshadow.at>
2016-03-13 07:39:18 +0100

    RpcController, make raiseError() be called statically

--- vendor/Thallium/Controllers/RpcController.php
+++ vendor/Thallium/Controllers/RpcController.php
@@ -26,11 +26,11 @@ class RpcController extends DefaultController
         global $router, $query;
 
         if (!isset($query->action)) {
-            $this->raiseError("No action specified!");
+            static::raiseError("No action specified!");
         }
 
         if (!$router->isValidRpcAction($query->action)) {
-            $this->raiseError("Invalid RPC action: ". htmlentities($query->action, ENT_QUOTES));
+            static::raiseError("Invalid RPC action: ". htmlentities($query->action, ENT_QUOTES));
             return false;
         }
 
@@ -72,11 +72,11 @@ class RpcController extends DefaultController
                 break;
             default:
                 if (!method_exists($this, 'performApplicationSpecifc')) {
-                    $this->raiseError("Unknown RPC action\n");
+                    static::raiseError("Unknown RPC action\n");
                     return false;
                 }
                 if (!$this->performApplicationSpecifc()) {
-                    $this->raiseError(__CLASS__ .'::performApplicationSpecifc() returned false!');
+                    static::raiseError(__CLASS__ .'::performApplicationSpecifc() returned false!');
                     return false;
                 }
                 break;
@@ -93,15 +93,15 @@ class RpcController extends DefaultController
 
         foreach ($input_fields as $field) {
             if (!isset($_POST[$field])) {
-                $this->raiseError(__METHOD__ ."(), '{$field}' isn't set in POST request!");
+                static::raiseError(__METHOD__ ."(), '{$field}' isn't set in POST request!");
                 return false;
             }
             if (empty($_POST[$field])) {
-                $this->raiseError(__METHOD__ ."(), '{$field}' is empty!");
+                static::raiseError(__METHOD__ ."(), '{$field}' is empty!");
                 return false;
             }
             if (!is_string($_POST[$field]) && !is_numeric($_POST[$field])) {
-                $this->raiseError(__METHOD__ ."(), '{$field}' is not from a valid type!");
+                static::raiseError(__METHOD__ ."(), '{$field}' is not from a valid type!");
                 return false;
             }
         }
@@ -111,37 +111,37 @@ class RpcController extends DefaultController
         $model = $_POST['model'];
 
         if (!$thallium->isValidId($id) && $id != 'flush') {
-            $this->raiseError(__METHOD__ .'(), \$id is invalid!');
+            static::raiseError(__METHOD__ .'(), \$id is invalid!');
             return false;
         }
 
         if (!$thallium->isValidGuidSyntax($guid) && $guid != 'flush') {
-            $this->raiseError(__METHOD__ .'(), \$guid is invalid!');
+            static::raiseError(__METHOD__ .'(), \$guid is invalid!');
             return false;
         }
 
         if (($model_name = $thallium->getModelByNick($model)) === false) {
-            $this->raiseError(get_class($thallium) .'::getModelNameByNick() returned false!');
+            static::raiseError(get_class($thallium) .'::getModelNameByNick() returned false!');
             return false;
         }
 
         /* special delete operation 'flush' */
         if ($id == 'flush' && $guid == 'flush') {
             if (($obj = $thallium->loadModel($model_name)) === false) {
-                $this->raiseError(get_class($thallium) .'::loadModel() returned false!');
+                static::raiseError(get_class($thallium) .'::loadModel() returned false!');
                 return false;
             }
 
             if (!method_exists($obj, 'flush')) {
-                $this->raiseError(__METHOD__ ."(), model {$model_name} does not provide a flush() method!");
+                static::raiseError(__METHOD__ ."(), model {$model_name} does not provide a flush() method!");
                 return false;
             }
             if (!$obj->permitsRpcActions('flush')) {
-                $this->raiseError(__METHOD__ ."(), model {$model_name} does not support flush-opertions!");
+                static::raiseError(__METHOD__ ."(), model {$model_name} does not support flush-opertions!");
                 return false;
             }
             if (!$obj->flush()) {
-                $this->raiseError(get_class($obj) .'::flush() returned false!');
+                static::raiseError(get_class($obj) .'::flush() returned false!');
                 return false;
             }
             print "ok";
@@ -149,22 +149,22 @@ class RpcController extends DefaultController
         }
 
         if (($obj = $thallium->loadModel($model_name, $id, $guid)) === false) {
-            $this->raiseError(get_class($thallium) .'::loadModel() returned false!');
+            static::raiseError(get_class($thallium) .'::loadModel() returned false!');
             return false;
         }
 
         if (!method_exists($obj, 'delete')) {
-            $this->raiseError(__METHOD__ ."(), model {$model_name} does not provide a delete() method!");
+            static::raiseError(__METHOD__ ."(), model {$model_name} does not provide a delete() method!");
             return false;
         }
 
         if (!$obj->permitsRpcActions('delete')) {
-            $this->raiseError(get_class($obj) .' does not permit "delete" via a RPC call!');
+            static::raiseError(get_class($obj) .' does not permit "delete" via a RPC call!');
             return false;
         }
 
         if (!$obj->delete()) {
-            $this->raiseError(get_class($obj) .'::delete() returned false!');
+            static::raiseError(get_class($obj) .'::delete() returned false!');
             return false;
         }
 
@@ -181,12 +181,12 @@ class RpcController extends DefaultController
         );
 
         if (!isset($_POST['content'])) {
-            $this->raiseError('No content requested!');
+            static::raiseError('No content requested!');
             return false;
         }
 
         if (!in_array($_POST['content'], $valid_content)) {
-            $this->raiseError('unknown content requested: '. htmlentities($_POST['content'], ENT_QUOTES));
+            static::raiseError('unknown content requested: '. htmlentities($_POST['content'], ENT_QUOTES));
             return false;
         }
 
@@ -201,7 +201,7 @@ class RpcController extends DefaultController
             return true;
         }
 
-        $this->raiseError("No content found!");
+        static::raiseError("No content found!");
         return false;
     }
 
@@ -219,39 +219,39 @@ class RpcController extends DefaultController
         );
 
         if (!isset($_POST['model'])) {
-            $this->raiseError('No model requested!');
+            static::raiseError('No model requested!');
             return false;
         }
 
         if (!in_array($_POST['model'], $valid_models)) {
-            $this->raiseError('unknown model requested: '. htmlentities($_POST['model'], ENT_QUOTES));
+            static::raiseError('unknown model requested: '. htmlentities($_POST['model'], ENT_QUOTES));
             return false;
         }
 
         if (!isset($_POST['id'])) {
-            $this->raiseError('id is not set!');
+            static::raiseError('id is not set!');
             return false;
         }
 
         $id = $_POST['id'];
 
         if (!$thallium->isValidId($id)) {
-            $this->raiseError('\$id is invalid');
+            static::raiseError('\$id is invalid');
             return false;
         }
 
         if (!isset($_POST['direction'])) {
-            $this->raiseError('direction is not set!');
+            static::raiseError('direction is not set!');
             return false;
         }
 
         if (!in_array($_POST['direction'], $valid_directions)) {
-            $this->raiseError('invalid direction requested: '. htmlentities($_POST['direction'], ENT_QUOTES));
+            static::raiseError('invalid direction requested: '. htmlentities($_POST['direction'], ENT_QUOTES));
             return false;
         }
 
         if (($id = $thallium->parseId($id)) === false) {
-            $this->raiseError('Unable to parse \$id');
+            static::raiseError('Unable to parse \$id');
             return false;
         }
 
@@ -265,7 +265,7 @@ class RpcController extends DefaultController
         }
 
         if (!isset($model) || empty($model)) {
-            $this->raiseError("Model not found: ". htmlentities($id->modek, ENT_QUOTES));
+            static::raiseError("Model not found: ". htmlentities($id->modek, ENT_QUOTES));
             return false;
         }
 
@@ -300,15 +300,15 @@ class RpcController extends DefaultController
 
         foreach ($input_fields as $field) {
             if (!isset($_POST[$field])) {
-                $this->raiseError(__METHOD__ ."(), '{$field}' isn't set in POST request!");
+                static::raiseError(__METHOD__ ."(), '{$field}' isn't set in POST request!");
                 return false;
             }
             if (empty($_POST[$field]) && $field != 'value') {
-                $this->raiseError(__METHOD__ ."(), '{$field}' is empty!");
+                static::raiseError(__METHOD__ ."(), '{$field}' is empty!");
                 return false;
             }
             if (!is_string($_POST[$field]) && !is_numeric($_POST[$field])) {
-                $this->raiseError(__METHOD__ ."(), '{$field}' is not from a valid type!");
+                static::raiseError(__METHOD__ ."(), '{$field}' is not from a valid type!");
                 return false;
             }
         }
@@ -319,17 +319,17 @@ class RpcController extends DefaultController
         $model = $_POST['model'];
 
         if (!(preg_match("/^([a-z]+)_([a-z_]+)$/", $key, $parts))) {
-            $this->raiseError(__METHOD__ ."() , key looks invalid!");
+            static::raiseError(__METHOD__ ."() , key looks invalid!");
             return false;
         }
 
         if ($id != 'new' && !is_numeric($id)) {
-            $this->raiseError(__METHOD__ ."(), id is invalid!");
+            static::raiseError(__METHOD__ ."(), id is invalid!");
             return false;
         }
 
         if (!$thallium->isValidModel($model)) {
-            $this->raiseError(__METHOD__ ."(), scope contains an invalid model ({$model})!");
+            static::raiseError(__METHOD__ ."(), scope contains an invalid model ({$model})!");
             return false;
         }
 
@@ -338,23 +338,23 @@ class RpcController extends DefaultController
         }
 
         if (($model_name = $thallium->getModelByNick($model)) === false) {
-            $this->raiseError(get_class($thallium) .'::getModelNameByNick() returned false!');
+            static::raiseError(get_class($thallium) .'::getModelNameByNick() returned false!');
             return false;
         }
 
         if (!($obj = $thallium->loadModel($model_name, $id))) {
-            $this->raiseError(__METHOD__ ."(), failed to load {$model}!");
+            static::raiseError(__METHOD__ ."(), failed to load {$model}!");
             return false;
         }
 
         // check if model permits RPC updates
         if (!$obj->permitsRpcUpdates()) {
-            $this->raiseError(__METHOD__ ."(), model {$model} denys RPC updates!");
+            static::raiseError(__METHOD__ ."(), model {$model} denys RPC updates!");
             return false;
         }
 
         if (!$obj->permitsRpcUpdateToField($key)) {
-            $this->raiseError(__METHOD__ ."(), model {$model} denys RPC updates to field {$key}!");
+            static::raiseError(__METHOD__ ."(), model {$model} denys RPC updates to field {$key}!");
             return false;
         }
 
@@ -363,7 +363,7 @@ class RpcController extends DefaultController
         $obj->$key = stripslashes($value);
 
         if (!$obj->save()) {
-            $this->raiseError(get_class($obj) ."::save() returned false!");
+            static::raiseError(get_class($obj) ."::save() returned false!");
             return false;
         }
 
@@ -379,12 +379,12 @@ class RpcController extends DefaultController
             empty($_POST['messages']) ||
             !is_string($_POST['messages'])
         ) {
-            $this->raiseError(__METHOD__ .'(), no message submited!');
+            static::raiseError(__METHOD__ .'(), no message submited!');
             return false;
         }
 
         if (!$mbus->submit($_POST['messages'])) {
-            $this->raiseError(get_class($mbus) .'::submit() returned false!');
+            static::raiseError(get_class($mbus) .'::submit() returned false!');
             return false;
         }
 
@@ -397,12 +397,12 @@ class RpcController extends DefaultController
         global $mbus;
 
         if (($messages = $mbus->poll()) === false) {
-            $this->raiseError(get_class($mbus) .'::poll() returned false!');
+            static::raiseError(get_class($mbus) .'::poll() returned false!');
             return false;
         }
 
         if (!is_string($messages)) {
-            $this->raiseError(get_class($mbus) .'::poll() has not returned a string!');
+            static::raiseError(get_class($mbus) .'::poll() has not returned a string!');
             return false;
         }
 
@@ -415,7 +415,7 @@ class RpcController extends DefaultController
         global $thallium;
 
         if (!$thallium->processRequestMessages()) {
-            $this->raiseError(get_class($thallium) .'::processRequestMessages() returned false!');
+            static::raiseError(get_class($thallium) .'::processRequestMessages() returned false!');
             return false;
         }
 
