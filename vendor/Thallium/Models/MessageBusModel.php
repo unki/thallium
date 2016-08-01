4d5889924c67f6dd54387350262ed56c8802e83b
Andreas Unterkircher <unki@netshadow.net>
2016-08-01 21:46:57 +0200

    MessageBusModel, add annotations

--- vendor/Thallium/Models/MessageBusModel.php
+++ vendor/Thallium/Models/MessageBusModel.php
@@ -19,25 +19,47 @@
 
 namespace Thallium\Models ;
 
+/**
+ * MessageBusModel represents the internal message queue.
+ * Messages are the represented by MessageModels.
+ *
+ * @package Thallium\Models\MessageBusModel
+ * @subpackage Models
+ * @license AGPL3
+ * @copyright 2015-2016 Andreas Unterkircher <unki@netshadow.net>
+ * @author Andreas Unterkircher <unki@netshadow.net>
+ */
 class MessageBusModel extends DefaultModel
 {
+    /** @var string $model_table_name */
     protected static $model_table_name = 'message_bus';
+
+    /** @var string $model_column_prefix */
     protected static $model_column_prefix = 'msg';
+
+    /** @var bool $model_has_items */
     protected static $model_has_items = true;
+
+    /** @var string $model_items_model */
     protected static $model_items_model = 'MessageModel';
 
+    /**
+     * get messages associated with a specific session id
+     *
+     * @param string $session_id
+     * @return array|bool
+     * @throws \Thallium\Controller\ExceptionController
+     */
     public function getMessagesForSession($session_id)
     {
         global $db;
 
-        $messages = array();
-
-        if (empty($session_id)) {
-            static::raiseError(__METHOD__ .', \$session_id can not be empty!');
+        if (!isset($session_id) || empty($session_id) || !is_string($session_id)) {
+            static::raiseError(__METHOD__ .'(), $session_id parameter is invalid!');
             return false;
         }
 
-        $idx_field = static::$model_column_prefix ."_idx";
+        $messages = array();
 
         $sql = sprintf(
             "SELECT
@@ -58,33 +80,43 @@ class MessageBusModel extends DefaultModel
             static::$model_table_name
         );
 
-        if (!($sth = $db->prepare($sql))) {
-            static::raiseError(__METHOD__ .', failed to prepare query!');
+        try {
+            $sth = $db->prepare($sql);
+        } catch (\PDOException $e) {
+            static::raiseError(get_class($db) .'::prepare() failed!', false, $e);
+            return false;
+        } catch (\Exception $e) {
+            static::raiseError(get_class($db) .'::prepare() failed!', false, $e);
             return false;
         };
 
-        if (!$db->execute($sth, array($session_id))) {
-            $db->freeStatement($sth);
-            static::raiseError(__METHOD__ .', failed to execute query!');
+        try {
+            $db->execute($sth, array($session_id));
+        } catch (\PDOException $e) {
+            static::raiseError(get_class($db) .'::execute() failed!', false, $e);
             return false;
-        }
-
+        } catch (\Exception $e) {
+            static::raiseError(get_class($db) .'::execute() failed!', false, $e);
+            return false;
+        };
+       
         while ($row = $sth->fetch()) {
             if (!isset($row->msg_idx) || empty($row->msg_idx) ||
                 !isset($row->msg_guid) || empty($row->msg_guid)
             ) {
                 $db->freeStatement($sth);
-                static::raiseError(__METHOD__ .', message returned from query is incomplete!');
+                static::raiseError(__METHOD__ .'(), message returned from query is incomplete!');
+                return false;
             }
 
             try {
-                $message = new MessageModel(array(
+                $message = new \Thallium\Models\MessageModel(array(
                     'idx' => $row->msg_idx,
                     'guid' => $row->msg_guid
                 ));
             } catch (\Exception $e) {
                 $db->freeStatement($sth);
-                static::raiseError('Failed to load MessageModel!');
+                static::raiseError(__METHOD__ .'(), failed to load MessageModel!', false, $e);
                 return false;
             }
 
@@ -95,13 +127,18 @@ class MessageBusModel extends DefaultModel
         return $messages;
     }
 
+    /**
+     * get messages that are designated to the framework.
+     *
+     * @param none
+     * @return array|bool
+     * @throws \Thallium\Controller\ExceptionController
+     */
     public function getServerRequests()
     {
         global $db;
 
         $messages = array();
-
-        $idx_field = static::column('idx');
 
         $sql = sprintf(
             "SELECT
@@ -118,26 +155,42 @@ class MessageBusModel extends DefaultModel
             static::$model_table_name
         );
 
-        if (!($result = $db->query($sql))) {
-            static::raiseError(__METHOD__ .', failed to query database!');
+        try {
+            $sth = $db->prepare($sql);
+        } catch (\PDOException $e) {
+            static::raiseError(get_class($db) .'::prepare() failed!', false, $e);
+            return false;
+        } catch (\Exception $e) {
+            static::raiseError(get_class($db) .'::prepare() failed!', false, $e);
             return false;
         };
 
-        while ($row = $result->fetch()) {
+        try {
+            $db->execute($sth);
+        } catch (\PDOException $e) {
+            static::raiseError(get_class($db) .'::execute() failed!', false, $e);
+            return false;
+        } catch (\Exception $e) {
+            static::raiseError(get_class($db) .'::execute() failed!', false, $e);
+            return false;
+        };
+ 
+        while ($row = $sth->fetch()) {
             if (!isset($row->msg_idx) || empty($row->msg_idx) ||
                 !isset($row->msg_guid) || empty($row->msg_guid)
             ) {
                 $db->freeStatement($sth);
-                static::raiseError(__METHOD__ .', message returned from query is incomplete!');
+                static::raiseError(__METHOD__ .'(), message returned from query is incomplete!');
+                return false;
             }
 
             try {
-                $message = new MessageModel(array(
+                $message = new \Thallium\Models\MessageModel(array(
                     'idx' => $row->msg_idx,
                     'guid' => $row->msg_guid
                 ));
             } catch (\Exception $e) {
-                static::raiseError('Failed to load MessageModel!');
+                static::raiseError(__METHOD__ .'(), failed to load MessageModel!', false, $e);
                 return false;
             }
 
@@ -147,6 +200,13 @@ class MessageBusModel extends DefaultModel
         return $messages;
     }
 
+    /**
+     * remove expired messages from message bus queue.
+     *
+     * @param int $timeout
+     * @return bool
+     * @throws \Thallium\Controller\ExceptionController
+     */
     public function deleteExpiredMessages($timeout)
     {
         global $db;
@@ -165,15 +225,34 @@ class MessageBusModel extends DefaultModel
             WHERE
                 UNIX_TIMESTAMP(msg_submit_time) < ?";
 
-        if (!($sth = $db->prepare($sql))) {
-            static::raiseError(__METHOD__ .', failed to prepare query!');
+        try {
+            $sth = $db->prepare($sql);
+        } catch (\PDOException $e) {
+            static::raiseError(get_class($db) .'::prepare() failed!', false, $e);
+            return false;
+        } catch (\Exception $e) {
+            static::raiseError(get_class($db) .'::prepare() failed!', false, $e);
+            return false;
+        };
+
+        if (!isset($sth) ||
+            empty($sth) ||
+            !is_object($sth) ||
+            !is_a($sth, 'PDOStatement')
+        ) {
+            static::raiseError(get_class($db) ."::prepare() returned invalid data!");
             return false;
         }
 
-        if (!($db->execute($sth, array($oldest)))) {
-            static::raiseError(__METHOD__ .', failed to execute query!');
+        try {
+            $db->execute($sth, array($oldest));
+        } catch (\PDOException $e) {
+            static::raiseError(get_class($db) .'::execute() failed!', false, $e);
             return false;
-        }
+        } catch (\Exception $e) {
+            static::raiseError(get_class($db) .'::execute() failed!', false, $e);
+            return false;
+        };
 
         return true;
     }
