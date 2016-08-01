171ad6c7704575527bd568a45df35cd7cc0fa34a
Andreas Unterkircher <unki@netshadow.net>
2016-08-01 21:48:24 +0200

    AuditLogModel, add annotations

--- vendor/Thallium/Models/AuditLogModel.php
+++ vendor/Thallium/Models/AuditLogModel.php
@@ -19,11 +19,27 @@
 
 namespace Thallium\Models ;
 
+/**
+ * Represents the whole audit log.
+ *
+ * @package Thallium\Models\AuditLogModel
+ * @subpackage Models
+ * @license AGPL3
+ * @copyright 2015-2016 Andreas Unterkircher <unki@netshadow.net>
+ * @author Andreas Unterkircher <unki@netshadow.net>
+ */
 class AuditLogModel extends DefaultModel
 {
+    /** @var string $model_table_name */
     protected static $model_table_name = 'audit';
+
+    /** @var string $model_column_prefix */
     protected static $model_column_prefix = 'audit';
+
+    /** @var bool $model_has_items */
     protected static $model_has_items = true;
+
+    /** @var string $model_items_model */
     protected static $model_items_model = 'AuditEntryModel';
 }
 
